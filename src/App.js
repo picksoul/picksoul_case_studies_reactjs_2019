2e13114d737c986cd03aafa122617065203ca965
Peter Birlem <peterbirlem@Sweet-Baby-Jesus-2.local>
2019-04-08 22:46:26 -0600

    added react router, built out image folders and heirarchies, added intital placeholder imgs to index.js, added styles.js and began moving over pertinent styles to components, added images to correlating image files under ./img p.birlem@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,28 +1,172 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'typeface-roboto';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to <code>picksoul</code> case study portfolio
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+//import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import logo from './img/logo_img/ps_orange.svg';
+import cereal from './img/home_img/cereal.svg';
+import home_heart_and_banner from './img/home_img/home_heart_and_banner.svg';
+
+
+console.log(logo); // /logo.84287d09.svg
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
+  },
+  layout: {
+    width: 'auto',
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.Secondary,
+    padding: theme.spacing.unit * 6,
+  },
+});
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+function Album(props) {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <img src={logo} alt="Logo" />
+          <Typography variant="h6" color="inherit" noWrap>
+            PickSoul
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+                    <img src={cereal} alt="cereal" />
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            Peter Birlem
+          </Typography>
+            <Typography component="h3" variant="h4" align="center" color="textSecondary" gutterBottom>
+              UX Design & Front-End Engineering
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              100% Whole Brain Goodness
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={16} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Main call to action
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </div>
+        <Grid container spacing={24} justify="center">
+          <img src={cereal} alt="cereal" />
+        </Grid>
+        <Grid container spacing={24} justify="center">
+          <img src={home_heart_and_banner} alt="home_heart_and_banner" />
+        </Grid>
+        <div className={classNames(classes.layout, classes.cardGrid)}>
+        <Typography component="h3" variant="h4" align="center" color="textSecondary" gutterBottom>
+          Case Studies
+        </Typography>
+          {/* End hero unit */}
+          <Grid container spacing={40}>
+            {cards.map(card => (
+              <Grid item key={card} sm={6} md={4} lg={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="image: ../home_img/cereal.png" // eslint-disable-line max-len
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          KEEP IT SIMPLE. KEEP IT FRESH.
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Copyright 2019 | PICKSOUL | DENVER, COLORADO
+        </Typography>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
 }
 
-export default App;
+Album.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Album);
---
+++ src/styles.js
@@ -0,0 +1,24 @@
+
+const Header = {
+  color: "white",
+  fontSize: "22px"
+}
+
+const Body = {
+  padding: "10px 20px",
+  textAlign: "center",
+  color: "white",
+  backgroundColor: 'black',
+  fontSize: "22px"
+}
+
+const ErrorMessage = {
+  color: "white",
+  fontSize: "13px"
+}
+
+const styles = {
+  TodoComponent: TodoComponent,
+  Header: Header,
+  ErrorMessage: ErrorMessage
+}
