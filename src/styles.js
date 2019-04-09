2a814f2789ec5b6ae05ae90424e80cb918f2c590
Peter Birlem <peterbirlem@Sweet-Baby-Jesus-2.local>
2019-04-09 10:52:38 -0600

    adding updated styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,9 @@
-
-const Header = {
-  padding: "10px 20px",
-  textAlign: "center",
-  color: "white",
-  fontSize: "22px"
+const TodoComponent = {
+  width: "300px",
+  margin: "30px auto",
+  backgroundColor: "#44014C",
+  minHeight: "200px",
+  boxSizing: "border-box"
 }
 
 const Header = {
